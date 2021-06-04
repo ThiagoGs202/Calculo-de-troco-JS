e9b95ebe8a7adac23609e7a12e27674394efd434
Thiago Gomes <80716962+ThiagoGs202@users.noreply.github.com>
2021-06-04 18:25:42 -0300

    Update troco.js

--- troco.js
+++ troco.js
@@ -1,13 +1,18 @@
-function calculaNotas(troco) {
-    var notas = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10];
-    var texto = '';
-    for (var x = 0; x < notas.length; x++) {
-        if (troco >= notas[x]) {
-            var div = Math.floor(troco / notas[x]);
-            texto += div + " notas de " + notas[x] + "\n";
-            troco -= div * notas[x];
-        }
+var notas    = [100,50,20,10,5,2,1,0.50,0.25,0.10,0.05];
+var troco    = 487.96;
 
-    }
-    return texto;
+function calcularTroco() {
+   trocado = new Array();	
+ 	
+   for (x in notas) {
+       if (notas[x] > troco) continue;
+       
+       var quantidadeCedula = parseInt(troco / notas[x]);
+       trocado.push([quantidadeCedula, notas[x]]);
+       
+       troco = troco - (quantidadeCedula * notas[x]);
+   }
+   
+   return trocado;
 }
+console.log(calcularTroco());
